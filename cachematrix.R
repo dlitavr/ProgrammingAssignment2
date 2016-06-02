9daf62a6f42565a937814ac6fcec147c98570c6b
dlitavr <dlitavr@yahoo.com>
2016-06-02 18:31:25 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to optimize multiple inversitions
+## for the same matrix by caching inversion result.
+## First "cached" matrix has to be created based on a matrix
+## with makeCacheMatrix() and then cacheSolve inverts it and caches
+## the inversion for the first time it is called.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a helper object, which stores a matrix
+## and its inversion and provides access methods for both
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## returns a list of get/set methods to access a matrix
+  ## and its inversion
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) inverse <<- inv
+  getinverse <- function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is similar to solve function - it inverts a matrix
+## but it also caches the inversion to optimize future calls to find
+## another inversion for the same matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if( !is.null(inv) ) {
+    ## This matrix has been already inversed - retrieving from cache
+    message("Returning cached inverted matrix...")
+    return(inv)
+  }
+  m <- x$get()
+  inv <- solve(m, ...)
+  ## Storing result in cache for future use
+  x$setinverse(inv)
+  inv
 }
